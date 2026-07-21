d25341e1448be23b4b90233a218044411e47e72b
yejunian <leeye51456@gmail.com>
2026-07-21 21:03:01 +0900

    🔧 config: Prettier 설정 및 포매팅 반영

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
-	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'none',
-	printWidth: 100,
-	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-	tailwindStylesheet: './src/routes/layout.css'
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+  tailwindStylesheet: "./src/routes/layout.css",
 };
 
 export default config;
